4f14ada6ea6521a7fdd6950ad564a65815cc1508
Mauricio Matango <Mauriciomatango_@hotmail.com>
2021-06-10 14:53:55 -0500

    Variables react

--- 03 counter-app/src/PrimeraApp.jsx
+++ 03 counter-app/src/PrimeraApp.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React  from 'react'
+//import React, { Fragment } from 'react'
 
 
 const PrimeraApp = ()=>{
     
-    return <h1>Hola Mundo</h1>
+     const saludo = 'Hola mundo ';
+    //const saludo = 123;
+    // const saludo = {
+    //     nombre : 'Mauricio',
+    //     edad: 21
+    
+    return (
+        <>
+            <h1>{saludo}</h1>
+            {/* <pre>{ JSON.stringify(saludo, null,3) }</pre> */}
+            <p>Mi primera Aplicacion</p>
+        </>
+        
+    );
+    
 }
 
 export default PrimeraApp;
